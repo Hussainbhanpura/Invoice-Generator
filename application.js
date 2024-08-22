b35bfefdec291ec2eef2dce663634e89340cf68f
Hussainbhanpura <108626978+Hussainbhanpura@users.noreply.github.com>
2024-08-22 17:50:37 +0530

    first

---
+++ application.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const conversionController = require("./src/controllers/conversion2.js");
+const path = require('path');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 4000;
+
+class Application {
+    static async main() {
+        const app = express();
+        app.use(express.json());
+        app.use(cors());
+
+        // Use the already defined __dirname and __filename
+        app.get('/', (req, res) => {
+            const filePath = path.join(__dirname, '../invoice/index.html');
+            res.sendFile(filePath);
+        });
+
+        app.use('/conversion', conversionController);
+
+        app.listen(PORT, () => {
+            console.log(`Application listening on port ${PORT}`);
+        });
+    }
+}
+
+module.exports = Application;
